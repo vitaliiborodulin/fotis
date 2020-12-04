1ff945ed698e93374e4a8b5c496f899c2d7e7e4c
Vitalii Borodulin <vitaliiborodulin@yandex.ru>
2020-12-04 15:05:24 +0300

    all work done

---
+++ gulpfile.js
@@ -0,0 +1,89 @@
+let fileswatch = 'html,htm,txt,json,md,woff2' // List of files extensions for watching & hard reload
+
+const { src, dest, parallel, series, watch } = require('gulp')
+const browserSync  = require('browser-sync').create()
+const webpack      = require('webpack-stream')
+const sass         = require('gulp-sass')
+const rigger       = require('gulp-rigger')
+const htmlmin      = require('gulp-htmlmin');
+const autoprefixer = require('gulp-autoprefixer')
+const rename       = require('gulp-rename')
+const imagemin     = require('gulp-imagemin')
+const newer        = require('gulp-newer')
+const del          = require('del')
+
+function browsersync() {
+	browserSync.init({
+		server: { baseDir: 'app/' },
+		notify: false,
+		online: true
+	})
+}
+
+function scripts() {
+	return src('app/js/app.js')
+	.pipe(webpack({
+		mode: 'production',
+		module: {
+			rules: [
+				{
+					test: /\.(js)$/,
+					exclude: /(node_modules)/,
+					loader: 'babel-loader',
+					query: {
+						presets: ['@babel/env']
+					}
+				}
+			]
+		}
+	})).on('error', function handleError() {
+		this.emit('end')
+	})
+	.pipe(rename('app.min.js'))
+	.pipe(dest('app/js'))
+	.pipe(browserSync.stream())
+}
+
+function html() {
+	return src('app/html/*.html')
+	.pipe(rigger())
+	.pipe(htmlmin({ collapseWhitespace: true }))
+	.pipe(dest('app'))
+	.pipe(browserSync.stream())
+}
+
+function styles() {
+	return src('app/sass/main.scss')
+	.pipe(sass({ outputStyle: 'compressed' }))
+	.pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true }))
+	.pipe(rename('app.min.css'))
+	.pipe(dest('app/css'))
+	.pipe(browserSync.stream())
+}
+
+function images() {
+	return src('app/img/src/**/*')
+	.pipe(newer('app/img/dest'))
+	.pipe(imagemin())
+	.pipe(dest('app/img/dest'))
+}
+
+function cleanimg() {
+	return del('app/img/dest/**/*', { force: true })
+}
+
+function startwatch() {
+	watch('app/html/**/*.html', { usePolling: true }, html)
+	watch('app/sass/**/*', { usePolling: true }, styles)
+	watch(['app/js/**/*.js', '!app/js/**/*.min.js'], { usePolling: true }, scripts)
+	watch('app/img/src/**/*.{jpg,jpeg,png,webp,svg,gif}', { usePolling: true }, images)
+	watch(`app/**/*.{${fileswatch}}`, { usePolling: true }).on('change', browserSync.reload)
+}
+
+exports.assets   = series(cleanimg, scripts, images)
+exports.html     = html
+exports.scripts  = scripts
+exports.styles   = styles
+exports.images   = images
+exports.cleanimg = cleanimg
+exports.default  = series(html, scripts, images, styles, parallel(browsersync, startwatch))
